4dc9d0ee0e2b5e89ce7aa23d62090deb4183d592
annaernst12345 <49048470+annaernst12345@users.noreply.github.com>
2019-12-04 19:21:26 -0600

    Update MainTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTest.java
@@ -1,8 +1,18 @@
+
+Autonomous(name="MainTest");
+public class MainTest extends LinearOpMode {
 HardwareRobot robot = new HardwareRobot();
+MiveBot move = new MoveBot(robot);
 
 @Override
 public void runOpMode()
 {
 robot.init;
 
+move.drive
 waitForStart();
+move.drive(move.motoPower(0.4), 1000);
+  
+}
+  
+}
